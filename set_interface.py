e91800e115cd660da4ef1b9fdab2ec4efd51ad58
Vasiliy <vasunekhin@gmail.com>
2022-08-25 10:57:35 +0300

    Specify commit message

--- set_interface.py
+++ set_interface.py
@@ -3,5 +3,19 @@ from other_classes import *
 
 class SetUI(UI):
     main_frame = Frame()
-    portal_autosave_button = ClassicButton(text='Настройка автосейва')
-    portal_color_button = ClassicButton(text='Настройка краски')
+    portal_autosave_button = ClassicButton(main_frame, text='Настройка автосейва')
+    portal_color_button = ClassicButton(main_frame, text='Настройка краски')
+    portal_language_button = ClassicButton(main_frame, text='Выбор языка')
+    portal_other_button = ClassicButton(main_frame, text='Прочее')
+    leave_button = ClassicButton(text='Выйти')
+
+    def place(self):
+        SetUI.main_frame.pack()
+        packing_portals_list = [[SetUI.portal_autosave_button, SetUI.portal_color_button],
+                                [SetUI.portal_language_button, SetUI.portal_other_button]]
+        for i in range(len(packing_portals_list)):
+            for j in range(len(packing_portals_list[i])):
+                packing_portals_list[i][j].grid(column=i, row=j, padx=2, pady=1)
+        SetUI.leave_button.pack()
+
+
