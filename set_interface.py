913aa59612a6d4b32904abd169df1b90313cb7e4
Vasiliy <vasunekhin@gmail.com>
2022-08-25 10:45:07 +0300

    Specify commit message

---
+++ set_interface.py
@@ -0,0 +1,7 @@
+from widget_classes import *
+from other_classes import *
+
+class SetUI(UI):
+    main_frame = Frame()
+    portal_autosave_button = ClassicButton(text='Настройка автосейва')
+    portal_color_button = ClassicButton(text='Настройка краски')
